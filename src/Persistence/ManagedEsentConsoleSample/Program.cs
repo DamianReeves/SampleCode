82bccb3d864516d0bfd574f83e24912586b13d7a
Damian Reeves <damreev@gmail.com>
2013-01-16 10:51:35 -0500

    Evaluating ManagedEsent as a Persistence medium

---
+++ src/Persistence/ManagedEsentConsoleSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Isam.Esent.Collections.Generic;
+using Persistence.Entities;
+
+namespace ManagedEsentConsoleSample {
+    class Program {
+        static void Main(string[] args) {
+            var path = Path.GetDirectoryName(typeof(Program).GetType().Assembly.Location);
+            var students = new PersistentDictionary<int, Student>(path); 
+            
+            while (true) {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.BackgroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("1) Create a student.");
+
+                if (students.Count > 0) {
+                    Console.WriteLine("2) List students.");
+                }
+            
+                Console.WriteLine("Q. Quit");
+
+                var line = Console.ReadLine();
+                if (String.Equals(line, "Q")) {
+                    break;
+                }
+          }
+        }
+    }
+}
---
+++ src/Persistence/Persistence.Entities/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Entities {
+    [DataContract]
+    public class Student {
+        [DataMember]
+        public int StudentId { get; set; }
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+    }
+
+    [DataContract]
+    public class Class {
+        public int ClassId { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Description { get; set; }
+        public IList<ClassMember> Members { get; set; }
+    }
+
+    [DataContract]
+    public class ClassMember {
+        public int ClassMemberId { get; set; }
+        public int ClassId { get; set; }
+        public int StudentId { get; set; }
+        public virtual Class Class { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
